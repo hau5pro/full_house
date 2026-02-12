08c7e578dcec4405b19a6d1783cc07efa3c82cfe
House Y <h4u5d3v@gmail.com>
2026-02-12 12:17:22 -0500

    remark fractions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { remarkFractions } from './src/plugins/remark-fractions.ts';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cookbook.hau5.pro',
   image: {
     domains: ['cookbookcdn.hau5.pro'],
+  },
+  markdown: {
+    remarkPlugins: [remarkFractions],
   },
 });
---
+++ src/plugins/remark-fractions.ts
@@ -0,0 +1,38 @@
+import { visit } from "unist-util-visit";
+
+const fractions: Record<string, string> = {
+  "1/2": "\u00BD",
+  "1/3": "\u2153",
+  "2/3": "\u2154",
+  "1/4": "\u00BC",
+  "3/4": "\u00BE",
+  "1/5": "\u2155",
+  "2/5": "\u2156",
+  "3/5": "\u2157",
+  "4/5": "\u2158",
+  "1/6": "\u2159",
+  "5/6": "\u215A",
+  "1/8": "\u215B",
+  "3/8": "\u215C",
+  "5/8": "\u215D",
+  "7/8": "\u215E",
+};
+
+const pattern = new RegExp(
+  `(?<=^|\\s)(\\d+\\s+)?(${Object.keys(fractions)
+    .map((k) => k.replace("/", "\\/"))
+    .join("|")})(?=\\s|$)`,
+  "g",
+);
+
+export function remarkFractions() {
+  return (tree: any) => {
+    visit(tree, "text", (node: any) => {
+      node.value = node.value.replace(
+        pattern,
+        (_match: string, whole: string | undefined, frac: string) =>
+          (whole ?? "") + fractions[frac],
+      );
+    });
+  };
+}
