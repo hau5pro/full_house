e814598198a2d249aa5cb4a29f75955e22bc58e4
House Y <h4u5d3v@gmail.com>
2026-02-13 11:04:36 -0500

    search with pagefind

--- TODO.md
+++ TODO.md
@@ -1,2 +1,1 @@
 - Add content
-- Search
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import pagefind from 'astro-pagefind';
 import { remarkFractions } from './src/plugins/remark-fractions.ts';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cookbook.hau5.pro',
+  integrations: [pagefind()],
   image: {
     domains: ['cookbookcdn.hau5.pro'],
   },
