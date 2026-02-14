4a4a4694ff5bbe92aee12e18442e218f0addae41
House Y <h4u5d3v@gmail.com>
2026-02-14 16:44:51 -0500

    specify category order

--- src/infrastructure/constants.ts
+++ src/infrastructure/constants.ts
@@ -1,1 +1,16 @@
 export const UNCATEGORIZED = "Uncategorized";
+
+export const CATEGORY_ORDER = [
+  "meals",
+  "sides",
+  "sauces",
+  "dips",
+  "seasonings",
+  "basics",
+];
+
+export function sortCategories(categories: string[]): string[] {
+  const known = CATEGORY_ORDER.filter((c) => categories.includes(c));
+  const extra = categories.filter((c) => !CATEGORY_ORDER.includes(c)).sort();
+  return [...known, ...extra];
+}
